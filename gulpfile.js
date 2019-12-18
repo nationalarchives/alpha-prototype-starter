8a9628dc039f592986a0809f13a1fe954b5da457
Gwyn Jones <m@gwnjns.com>
2019-12-18 12:26:54 +0000

    Add Gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,15 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const { series } = require('gulp');
+const { watch } = require('gulp');
 
 function styles(cb) {
     gulp.src('./app/static/styles/src/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('./app/static/styles/dist/'))
     cb();
 }
 
-exports.build = series(styles);
-
-
-
+exports.default = function () {
+    watch('./app/static/styles/src/*.scss', styles);
+};
